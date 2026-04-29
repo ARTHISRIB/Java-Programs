2c1f5eebfb13d84211e6b2195d49bf62737bd840
ARTHISRIB <arthisri275@gmail.com>
2026-04-29 11:28:10 +0530

    ELigibility to vote
    Signed-off-by: ARTHISRIB <arthisri275@gmail.com>

---
+++ sample3.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] args) {
+        int age = 21;
+
+        if(age >=18){
+            System.out.println("Eligible to vote");
+        }
+        else{
+            System.out.println("Not eligible");
+        }
+        
+    }
+}
