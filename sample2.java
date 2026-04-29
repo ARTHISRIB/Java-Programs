16ebf000f0388412f7c06a71252a8a8028858381
ARTHISRIB <arthisri275@gmail.com>
2026-04-29 11:20:05 +0530

    next code
    Signed-off-by: ARTHISRIB <arthisri275@gmail.com>

---
+++ sample2.java
@@ -0,0 +1,21 @@
+class Main{
+    public static void main(String[] args) {
+        int grade = 79;
+        
+        if(grade==90){
+            System.out.println("A");
+        }
+        else if(grade>=80){
+            System.out.println("B");
+        }
+        else if(grade>=70){
+            System.out.println("C");
+        }
+        else if(grade>=60){
+            System.out.println("D");
+        }
+        else{
+            System.out.println("F");
+        }
+    }
+}
