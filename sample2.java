5a6fd61e8969ec177754b44e6ff57ea090cc4048
ARTHISRIB <arthisri275@gmail.com>
2026-05-01 13:53:51 +0530

    constructors in java

--- sample2.java
+++ sample2.java
@@ -2,20 +2,12 @@ class Main{
     public static void main(String[] args) {
         int grade = 79;
         
-        if(grade==90){
-            System.out.println("A");
+        if(grade>=40){
+            System.out.println("PASS");
         }
-        else if(grade>=80){
-            System.out.println("B");
-        }
-        else if(grade>=70){
-            System.out.println("C");
-        }
-        else if(grade>=60){
-            System.out.println("D");
-        }
+        
         else{
-            System.out.println("F");
+            System.out.println("FAIL");
         }
     }
 }
